bfbb02012fda063a4968c8af94f9892aee3852e2
Wait What <barkdogmc@gmail.com>
2020-02-22 19:38:57 +0200

    0.5.0 Improved emoji insertion, cleaned up code

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 
 const faces = ['(*^ω^)', '(◕‿◕✿)', '(◕ᴥ◕)', 'ʕ•ᴥ•ʔ', 'ʕ￫ᴥ￩ʔ', '(*^.^*)', 'owo', '(｡♥‿♥｡)', 'uwu', '(*￣з￣)', '>w<', '^w^', '(つ✧ω✧)つ', '(/ =ω=)/'];
 
-const owoString = (string) => {
-    string = string.replace(/(?:l|r)/g, 'w');
-    string = string.replace(/(?:L|R)/g, 'W');
-    string = string.replace(/n([aeiou])/g, 'ny$1');
-    string = string.replace(/N([aeiou])/g, 'Ny$1');
-    string = string.replace(/N([AEIOU])/g, 'Ny$1');
-    string = string.replace(/ove/g, 'uv');
-    string = string.replace(/!+/g, ` ${faces[Math.floor(Math.random() * faces.length)]} `);
+module.exports = string => {
+    while (string.match(/!+/)) {
+        string = string.replace(/!+/, ` ${faces[Math.floor(Math.random() * faces.length)]} `)
+    }
 
-    return string;
-};
-
-module.exports = owoString;
+    return string
+        .replace(/(?:l|r)/g, 'w')
+        .replace(/(?:L|R)/g, 'W')
+        .replace(/n([aeiou])/g, 'ny$1')
+        .replace(/N([aeiou])/g, 'Ny$1')
+        .replace(/N([AEIOU])/g, 'NY$1')
+        .replace(/ove/g, 'uv')
+        .replace(/OVE/g, 'UV');
+}
